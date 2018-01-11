ac3b42d523766c2b8e11f85ae26c72217813845f
Askhat <askhat.adhamov@gmail.com>
2018-01-11 19:28:57 +0600

    first lab: 3 and 4

---
+++ week 01/lab 1/3/3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Rectangle
+    {
+        string color;
+        int width;
+        int height;
+        int area;
+        int perimeter;
+
+        public void findArea()
+        {
+            area = width * height;
+        }
+
+        public void findPer()
+        {
+            perimeter = 2 * (width + height);
+        }
+
+        public Rectangle()
+        {
+            color = "black";
+            width = 1;
+            height = 1;
+            findArea();
+            findPer();
+        }
+
+        public Rectangle(string color, int width, int height)
+        {
+            this.color = color;
+            this.width = width;
+            this.height = height;
+            findArea();
+            findPer();
+        }
+        public override string ToString()
+        {
+            return "Color is " + color + "\nWidth = " + width + "\nHeight = " + height + "\nArea = " + area + "\nPerimeter = " + perimeter;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write, please, color, width, height of your Rectangle");
+            string c, line = Console.ReadLine();
+            string[] newline = line.Split(' ');
+            c = newline[0];
+            int a = int.Parse(newline[1]);
+            int b = int.Parse(newline[2]);
+
+            Rectangle r1 = new Rectangle();
+            Console.WriteLine("DEFAULD: \n" + r1);
+            Rectangle r2 = new Rectangle(c, a, b);
+            Console.WriteLine("\nYOUR: \n" + r2);
+
+            Console.WriteLine("\n Press button");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ week 01/lab 1/3_2/3_2/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Circle
+    {
+        public int radius;
+        public double area;
+        public int diameter;
+        public double circumference;
+        string color;
+
+        public Circle()
+        {
+            color = "black";
+            radius = 10;
+            findArea();
+            findDmt();
+            findCir();
+        }
+
+        public Circle(string color, int radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            findArea();
+            findDmt();
+            findCir();
+        }
+
+        public void findArea()
+        {
+            area = Math.PI * radius * radius;
+        }
+        
+        public void findDmt()
+        {
+            diameter = radius * 2;
+        }
+        public void findCir()
+        {
+            circumference = Math.PI * 2 * radius;
+        }
+
+        public override string ToString()
+        {
+            return "Color is " + color + "\nRadius = " + radius + "\nArea = " + area + "\nCircumference = " + circumference + "\nDiameter = " + diameter;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write color, radius of your Circle");
+            string c, line = Console.ReadLine();
+            string[] newline = line.Split(' ');
+            c = newline[0];
+            int r = int.Parse(newline[1]);
+
+            Circle c1 = new Circle();
+            Console.WriteLine("DEFAULD: \n" + c1);
+            Circle c2 = new Circle(c, r);
+            Console.WriteLine("\nYOUR: \n" + c2);
+
+            Console.WriteLine("\n Press button");
+            Console.ReadKey();
+
+        }
+    }
+}
+
