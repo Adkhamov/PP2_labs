a7966ff27c3dbcdf1f6ebd081ea3834cba03e0c2
Askhat <askhat.adhamov@gmail.com>
2018-01-11 18:35:26 +0600

    First lab: 1 and 2

---
+++ week 01/lab 1/1.cs/1.cs/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _1.cs 
+{
+
+   
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string line = Console.ReadLine();       // ввод строки, содержащей числа и пробелы
+            string[] arr = line.Split(' ');         // запись строки в массив без пробелов
+            for (int i = 0; i < arr.Length; i++)    // пробегаемся по масиву
+            {
+                bool ok = true;                     // создаем переменную, отвечающую за простоту числа
+                if (int.Parse(arr[i]) == 1)
+                {
+                    continue;                       // если значение arr[i] = 1, то это не простое числа, пропускаем его
+                }
+                else for (int j = 2; j <= Math.Sqrt(int.Parse(arr[i])); j++) // пробегаемя по возможным множителям цифры с 2 до корня числа 
+                {
+                    
+                    if (int.Parse(arr[i]) % j == 0)
+                    {
+                        ok = false;                 // если число делит на j без остатка, оно не простое. пропускам его
+                        break;
+                    }
+                 
+                }
+                if (ok == true)                     // если значение ok не поменялось за все время, то число простое
+                {
+                    Console.Write(arr[i] + " ");    // выводим простые числа через пробел
+                }
+                
+            }
+            Console.Write("\nНажмите любую клавишу");
+            Console.ReadKey();                      // задержка экрана до нажатия любой кнопки
+        }
+    }
+}
---
+++ week 01/lab 1/2/2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.cs
+{
+    class Student
+    {
+        public string name;
+        public string sname;
+        public int age;
+        public double gpa;
+        public int course;
+        public string department;
+
+        public Student()
+        {
+            name = "Adkhamov";
+            sname = "Askhat";
+            department = "FIT";
+            age = 18;
+            course = 1;
+            gpa = 4;
+        }
+        public Student(string n, string s, string d, int a, int c, double g)
+        {
+            name = n;
+            sname = s;
+            department = d;
+            age = a;
+            course = c;
+            gpa = g;
+        }
+        public override string ToString()
+        {
+            return "name: " + name + "\nsname: " + sname + "\ndepartment: " + department + "\ncourse: " + course + "\nage: " + age + "\nGPA: " + gpa;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write name, surname, department, course, age, gpa, Please.");
+            string st = Console.ReadLine();
+            string[] st0 = st.Split(' ');
+            string n, s, d;
+            int a, c;
+            double g;
+            n = st0[0];
+            s = st0[1];
+            d = st0[2];
+            a = int.Parse(st0[3]);
+            c = int.Parse(st0[4]);
+            g = int.Parse(st0[5]);
+            
+            Student st1 = new Student();
+            Console.WriteLine("DEFAULD:\n" + st1);
+            Student st2 = new Student(n, s, d, c, a, g);
+            Console.WriteLine("YOUR:\n" + st2);
+            Console.WriteLine("\n Press button");
+            Console.ReadKey();
+        }
+    }
+}
